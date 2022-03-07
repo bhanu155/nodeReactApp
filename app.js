0058b4ca4ae6b793e243a0af9561943a9561ddda
CHANDRA BHANU <cbhanu.155@gmail.com>
2022-03-07 20:06:09 +0530

    project initiated

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server is running on port : ", PORT);
+});
+
+//POST endpoint
+app.post("/fetchData", (req, res) => {
+  console.log("body =  ", req.body);
+  let name = req.body.name;
+  let date = new Date(req.body.date);
+
+  const day = date.getDay();
+  const dayMap = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const message = "Hi " + name + ", you were born on a " + dayMap[day];
+
+  console.log(message);
+
+  res.send(JSON.stringify({ message: message }));
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("/fetchData", {
+        method: "POST",
+        headers: new Headers({
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        }),
+        body: `name=${name}&date=${date}`,
+      });
+      let resJson = await res.json();
+
+      console.log("response = ", resJson);
+
+      if (res.status === 200) {
+        setMessage(resJson.message);
+      } else {
+        setMessage("Invalid Input");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="date"
+          placeholder="date"
+          onChange={(e) => setDate(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
     </div>
   );
 }
